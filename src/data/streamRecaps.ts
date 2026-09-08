0634a9d0c05f91bfce86c2eb586ffc453328e025
ackey1007fw-coder <ackey1007fw@gmail.com>
2026-09-08 21:15:34 +0900

    content: add August 22 night stream recap and eight stills

--- src/data/streamRecaps.ts
+++ src/data/streamRecaps.ts
@@ -1,3 +1,4 @@
+import { streamRecap20260822Night } from "./streamRecap20260822Night.ts";
 import { streamRecap20260823Asa } from "./streamRecap20260823Asa.ts";
 import { streamRecap20260823Night } from "./streamRecap20260823Night.ts";
 import { streamRecap20260824Asa } from "./streamRecap20260824Asa.ts";
@@ -183,6 +184,7 @@ export const streamRecaps: StreamRecap[] = [
   streamRecap20260824Asa,
   streamRecap20260823Night,
   streamRecap20260823Asa,
+  streamRecap20260822Night,
   streamRecap20260821Day,
   streamRecap20260821Asa,
   streamRecap20260819Day,
